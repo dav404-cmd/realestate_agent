86df20231dff1ae535be31a32a1ebaba2746a7f2
nox map <hw19196@gmail.com>
2026-01-29 18:51:11 +0545

    Relocate: sifted apis/ into ui/backend/ ;[apis/data_querying]Update: switched data loading from fastapi startup to loading data throught lifespan in app.state.df
    added missing __init__.py

--- ui/backend/apis/data_querying.py
+++ ui/backend/apis/data_querying.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
-import pandas as pd
+from contextlib import asynccontextmanager
 
 from manage_db.db_manager import DbManager
 from manage_db.query import query_properties, PropertyQuery
@@ -9,26 +9,33 @@ from manage_db.query import query_properties, PropertyQuery
 from data.data_cleaner.to_json_safe import df_to_json_safe_records
 
 from utils.logger import get_logger
+
 api_log = get_logger("API_log")
 
-app = FastAPI()
-df : pd.DataFrame | None = None
-
-@app.on_event("startup")
-def load_data():
-    global df
+@asynccontextmanager
+async def lifespan(app:FastAPI):
     db = DbManager(table_name="jp_realestate")
     df = db.load_data()
     db.close_conn()
+
+    app.state.df = df
     api_log.info(f"Loaded {len(df)} rows into memory")
+
+    yield
+
+    api_log.info("Application shutdown complete")
+
+
+app = FastAPI(lifespan=lifespan)
+
 
 @app.post("/search")
 def search(q: PropertyQuery):
     api_log.info(f"Received query: {q}")
     try:
-        if df is None:
+        if app.state.df is None:
             return {"error":"Data not loaded"}
-        results = query_properties(df,q)
+        results = query_properties(app.state.df,q)
         return jsonable_encoder(df_to_json_safe_records(results))
     except Exception as e:
         api_log.exception("Search failed")
