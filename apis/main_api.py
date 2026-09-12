5e6736a4dd98ac065c1150ee7eec4a436a1ac291
nox map <hw19196@gmail.com>
2026-09-12 16:32:42 +0545

    [apis]update : made the api support the new domain in vercel via corsmiddleware , auth now redirects to the vercel frontend page ; removed most of the local host support for now

--- apis/main_api.py
+++ apis/main_api.py
@@ -21,8 +21,7 @@ app.add_middleware(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5500",
-        "http://127.0.0.1:5500"
+        "https://tsubonote.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
